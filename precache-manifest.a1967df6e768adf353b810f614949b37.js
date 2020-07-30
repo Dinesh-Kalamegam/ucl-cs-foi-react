c32e4357d97b1a8aa2fb9be7e07756702026eca3
Dinesh-Kalamegam <dinesh.kalamegam@gmail.com>
2020-07-30 20:09:40 +0100

    Updates

---
+++ precache-manifest.a1967df6e768adf353b810f614949b37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2214bbedc76415deafd5f2c51f459726",
+    "url": "/ucl-cs-foi-react/index.html"
+  },
+  {
+    "revision": "a9adcce4b5cd1183af23",
+    "url": "/ucl-cs-foi-react/static/css/main.3e0fa976.chunk.css"
+  },
+  {
+    "revision": "8ea94861d3523c76d9e8",
+    "url": "/ucl-cs-foi-react/static/js/2.32b6162c.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/ucl-cs-foi-react/static/js/2.32b6162c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9adcce4b5cd1183af23",
+    "url": "/ucl-cs-foi-react/static/js/main.e9f0571f.chunk.js"
+  },
+  {
+    "revision": "cad20ba0e00b0d407e55",
+    "url": "/ucl-cs-foi-react/static/js/runtime-main.c12bf38f.js"
+  }
+]);
